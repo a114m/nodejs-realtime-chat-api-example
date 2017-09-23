24fa02ae06b2aeaea801b9a5571490e8a171f532
allam <m@a114m.me>
2017-09-23 16:54:45 +0200

    socket.io initial implementation for RTM

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.resolve('views/index.html'));
 });
 
 module.exports = router;
